553992b7d5f1c7c12cc2be054cdfc64eaec5a24e
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-07-13 10:43:09 -0700

    debug infinite loop2

--- src/pages/LogoutPage.js
+++ src/pages/LogoutPage.js
@@ -5,7 +5,7 @@ export default function LogoutPage() {
   const store = authStore();
   useEffect(() => {
     store.logout();
-  });
+  }, []);
 
   return <h1>You are now logged out</h1>;
 }
--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -10,7 +10,7 @@ export default function NotesPage() {
   //Use Effect
   useEffect(() => {
     store.fetchNotes();
-  });
+  }, []);
 
   return (
     <div>
