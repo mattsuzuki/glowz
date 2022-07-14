3c55acae583f37370056c80e7e7f21cf040794d6
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-07-14 12:21:13 -0700

    styling more2

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,13 +5,16 @@ import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
     <div className="landing">
-      <h1>Welcome to GLOWZ</h1>
+      <h1>GLOWZ</h1>
+      <img className="logo" src="/assets/images/glowz.svg" />
       <h4>
         A platform to privately share your daily achievements, struggles, or any
         things you want to keep private
       </h4>
       <Link to="/notes">
-        <Button text="Enter GLOWZ" />
+        <div className="landbutton">
+          <Button text="Enter GLOWZ" />
+        </div>
       </Link>
     </div>
   );
